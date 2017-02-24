84fe87a97db7d4451b9047bfac723ee1ae2d5373
Manish Gautam <manish.gautam.india@gmail.com>
2017-02-25 00:39:04 +0530

    Initial Commit

---
+++ src/app/index.module.ts
@@ -0,0 +1,23 @@
+/// <reference path="../../typings/main.d.ts" />
+
+import { config } from './index.config';
+import { routerConfig } from './index.route';
+import { runBlock } from './index.run';
+import { MainController } from './main/main.controller';
+import { acmeNavbar } from '../app/components/navbar/navbar.directive';
+
+declare var malarkey: any;
+declare var moment: moment.MomentStatic;
+
+module simitu {
+  'use strict';
+
+  angular.module('simitu', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr'])
+    .constant('malarkey', malarkey)
+    .constant('moment', moment)
+    .config(config)
+    .config(routerConfig)
+    .run(runBlock)
+    .controller('MainController', MainController)
+    .directive('acmeNavbar', acmeNavbar)
+}
